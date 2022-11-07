81bf39714c57a7fb669792a2f800b73cc51efce1
samue <samue@DESKTOP-6S5KJ02>
2022-11-07 21:02:15 +0100

    Add project files.

---
+++ hazi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adatok = "10;5;7";
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                if (adatok[i] == ';')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(Convert.ToInt32(adatok[i].ToString()) * 2);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
